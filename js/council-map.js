164bbbf5a70349eb057aaff3e6d329bd95e05940
Jonathan Hollinger <jmholl5@gmail.com>
2015-02-18 16:09:44 -0500

    Update Map

--- js/council-map.js
+++ js/council-map.js
@@ -1,13 +1,37 @@
 var map;
 function initialize() {
+
   // Create a simple map.
   map = new google.maps.Map(document.getElementById('map-canvas'), {
     zoom: 13,
     center: {lat: 38.048876, lng: -84.499932}, 
   });
 
-  // Load a GeoJSON from the same server as our demo.
+
+  // Load a Council GeoJSON
   map.data.loadGeoJson('data/council.geojson');
+
+    var blue = {
+    fillColor: '#2A6FBD',
+    fillOpacity: 0.1,
+    strokeColor: '#2A6FBD',
+    strokeWeight: 2
+    };
+
+  map.data.setStyle(blue);
+
+
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+
+console.log("District " + GetURLParam("district"))
+
+function GetURLParam(sParam){
+var sPageURL = window.location.search.substring(1);
+var sURLVariables = sPageURL.split('&');
+for (var i = 0; i < sURLVariables.length; i++) 
+{
+var sParameterName = sURLVariables[i].split('=');
+if (sParameterName[0] == sParam) 
+   {return sParameterName[1];}}}
