8d7a633e925c826334f21af0359b3a9a5e7b3db6
Jonathan Hollinger <jmholl5@gmail.com>
2015-02-18 14:47:44 -0500

    Create Base Map

---
+++ js/council-map.js
@@ -0,0 +1,28 @@
+function initialize(){
+
+  var mapOptions = {
+    overviewMapControl:true,
+    rotateControl:true,
+    scaleControl:true,
+    mapTypeControl: true,
+    mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR, position:google.maps.ControlPosition.TOP_CENTER},
+    zoomControl: true,
+    zoomControlOptions: {style: google.maps.ZoomControlStyle.DEFAULT}
+    };
+
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  var blue = {
+    fillColor: '#2A6FBD',
+    fillOpacity: 0.2,
+    strokeColor: '#2A6FBD',
+    strokeWeight: 2
+    };
+
+  map.data.setStyle(blue);
+  
+  map.data.loadGeoJson('data/council.geojson')
+
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
