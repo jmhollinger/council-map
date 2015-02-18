0f5bf03e0288c7813d6d82f6cde74572d60b015f
Jonathan Hollinger <jmholl5@gmail.com>
2015-02-18 15:03:47 -0500

    Fix Base Map

--- js/council-map.js
+++ js/council-map.js
@@ -1,28 +1,13 @@
-function initialize(){
+var map;
+function initialize() {
+  // Create a simple map.
+  map = new google.maps.Map(document.getElementById('map-canvas'), {
+    zoom: 13,
+    center: {lat: 38.048876, lng: -84.499932}, 
+  });
 
-  var mapOptions = {
-    overviewMapControl:true,
-    rotateControl:true,
-    scaleControl:true,
-    mapTypeControl: true,
-    mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR, position:google.maps.ControlPosition.TOP_CENTER},
-    zoomControl: true,
-    zoomControlOptions: {style: google.maps.ZoomControlStyle.DEFAULT}
-    };
-
-  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
-  var blue = {
-    fillColor: '#2A6FBD',
-    fillOpacity: 0.2,
-    strokeColor: '#2A6FBD',
-    strokeWeight: 2
-    };
-
-  map.data.setStyle(blue);
-  
-  map.data.loadGeoJson('data/council.geojson')
-  
+  // Load a GeoJSON from the same server as our demo.
+  map.data.loadGeoJson('data/council.geojson');
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
